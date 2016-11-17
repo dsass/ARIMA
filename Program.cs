44c1dd7b39da2874a542a92a2737ffd773f84ad3
steffanigomez323 <steffanigomez323@gmail.com>
2016-11-17 12:43:19 -0500

    some preliminary code to read in data from a CSV file

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARIMA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("ERROR: Please specify a dataset contained in a .csv");
+                return;
+            }
+            string file = args[0];
+            ARIMA.dataprocessing.CSVReader reader = new ARIMA.dataprocessing.CSVReader();
+            string[] headers = reader.getHeaders(file);
+            List<String> data = reader.getData(0, file);
+        }
+    }
+}
---
+++ dataprocessing/CSVReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARIMA.dataprocessing
+{
+    class CSVReader
+    {
+        public string[] getHeaders(string filename)
+        {
+            System.IO.StreamReader filereader = null;
+            try
+            {
+                filereader = new System.IO.StreamReader(@filename);
+            } catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+            string header = filereader.ReadLine();
+            string[] headers = header.Split(',').Select(s => s.Trim()).Where(s => s != String.Empty).ToArray();
+            return headers;                    
+        }
+
+        public List<string> getData(int attrindex, string filename)
+        {
+            System.IO.StreamReader reader = null;
+            try
+            {
+                reader = new System.IO.StreamReader(@filename);
+            } catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+            string line = reader.ReadLine();
+            List<String> data = new List<String>();
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] values = line.Split(',').Select(s => s.Trim()).Where(s => s != String.Empty).ToArray();
+                data.Add(values[attrindex]);
+            }
+            return data;
+        }
+
+    }
+}
