866611d69951bdd7bdb0ea3af6dba1c4e06a65ea
steffanigomez323 <steffanigomez323@gmail.com>
2016-12-06 15:21:32 -0500

    adding ARIMATest

---
+++ ARIMATest/timeseries/math/linalg/ArrayManipulationTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ARIMATest.timeseries.math.linalg
+{
+    [TestClass]
+    public class ArrayManipulationTest
+    {
+        [TestMethod]
+        public void diffTest()
+        {
+            double[] x = { 1, 2, 4, 7, 0 };
+            double[] ans1 = ARIMA.timeseries.math.MathFunctions.diff(x);
+            Assert.AreEqual(System.Linq.Enumerable.SequenceEqual(ans1, new double[] { 1, 2, 3, -7 }), true);
+            Assert.AreEqual(System.Linq.Enumerable.SequenceEqual(ARIMA.timeseries.math.MathFunctions.diff(x, 2), new double[] { 1, 1, -10 }), true);
+        }
+    }
+}
---
+++ ARIMATest/timeseries/models/OLSTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ARIMA.timeseries.models;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace ARIMATest.timeseries.models
+{
+    [TestClass]
+    public class OLSTest
+    {
+        [TestMethod]
+        public void OLSinitTest()
+        {
+            Matrix<double> xdshort = Matrix<double>.Build.Dense(1, 6, 1.0);
+            Matrix<double> RHS = Matrix<double>.Build.Dense(6, 3, 1.0);
+            for (int i = 0; i < 6; i++)
+            {
+                RHS[i, 0] = i + 3;
+            }
+            //int startlag = 1;
+            OLS OLS_instance = new OLS(xdshort, RHS.SubMatrix(0, RHS.RowCount, 0, 1), "OLS");
+        }
+
+        [TestMethod]
+        public void OLSFitTest()
+        {
+            Matrix<double> xdshort = Matrix<double>.Build.Dense(1, 6, 1.0);
+            Matrix<double> RHS = Matrix<double>.Build.Dense(6, 3, 1.0);
+            for (int i = 0; i < 6; i++)
+            {
+                RHS[i, 0] = i + 3;
+            }
+            //int startlag = 1;
+            OLS OLS_instance = new OLS(xdshort, RHS.SubMatrix(0, RHS.RowCount, 0, 1), "OLS");
+            RegressionResults res = OLS_instance.fit();
+            Console.WriteLine(res.Adfstat);
+        }
+
+    }
+}
