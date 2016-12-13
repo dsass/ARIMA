5d011fa26de24e9ed9f3c0fe9a127acdf9a6d012
David <david.e.sasson@gmail.com>
2016-12-13 00:11:20 -0500

    added Transforms and ARIMA process

---
+++ cronos-ARMA/ABMath/ModelFramework/Transforms/ForecastTransform.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using ABMath.Forms;
+using ABMath.ModelFramework.Data;
+using ABMath.ModelFramework.Models;
+
+namespace ABMath.ModelFramework.Transforms
+{
+    [Serializable]
+    public class ForecastTransform : TimeSeriesTransformation, IExtraFunctionality
+    {
+        [Category("Parameter"), Description("Times at which to generate forecasts")]
+        public DateTime[] FutureTimes { get; set; }
+
+        public ForecastTransform()
+        {
+            FutureTimes = new DateTime[10];
+            for (int i=0 ; i<10 ; ++i)
+                FutureTimes[i] = new DateTime(2011, 1, (i + 1)); // default
+        }
+
+        public override int NumInputs()
+        {
+            return 2; // model and starting data
+        }
+
+        public override int NumOutputs()
+        {
+            return 1; // predictive means
+        }
+
+        public override string GetInputName(int index)
+        {
+            if (index == 0)
+                return "Model";
+            if (index == 1)
+                return "Starting Data";
+            throw new SocketException();
+        }
+
+        public override string GetOutputName(int index)
+        {
+            if (index == 0)
+                return "Predictive Mean";
+            throw new SocketException();
+        }
+
+        public override string GetDescription()
+        {
+            return "Forecasts";
+        }
+
+        public override string GetShortDescription()
+        {
+            return "Forecasts";
+        }
+
+        public override Icon GetIcon()
+        {
+            return null;
+        }
+
+        public override bool SetInput(int socket, object item, StringBuilder failMsg)
+        {
+            CheckInputsReady();
+            if (socket >= NumInputs())
+                throw new ArgumentException("Bad socket.");
+
+            if (socket == 0)
+                if (item as TimeSeriesModel == null)
+                {
+                    if (failMsg != null)
+                        failMsg.Append("The first input must be a model.");
+                    return false;
+                }
+            if (socket == 1)
+                if (item as TimeSeries == null)
+                    return false;
+
+            socketedInputs[socket] = item;
+
+            if (AllInputsValid())
+                Recompute();
+
+            return true;
+        }
+
+        public override void Recompute()
+        {
+            outputs = new List<TimeSeries>();
+            IsValid = false;
+
+            if (FutureTimes == null)
+                return;
+            if (FutureTimes.Length == 0)
+                return;
+
+            var tsm = GetInput(0) as UnivariateTimeSeriesModel;
+            var tsd = GetInput(1) as TimeSeries;
+
+            if (tsm == null || tsd == null)
+                return;
+
+            var fdt = new List<DateTime>();
+            foreach (DateTime dt in FutureTimes)
+                fdt.Add(dt);
+            var forecasts = tsm.BuildForecasts(tsd, fdt) as TimeSeriesBase<DistributionSummary>;
+            if (forecasts == null)
+                return;
+
+            var predictiveMeans = new TimeSeries();
+            for (int t = 0; t < forecasts.Count; ++t)
+                predictiveMeans.Add(forecasts.TimeStamp(t), forecasts[t].Mean, false);
+
+            outputs.Add(predictiveMeans);
+            IsValid = predictiveMeans.Count > 0;
+        }
+
+        public int NumAuxiliaryFunctions()
+        {
+            return 1;
+        }
+
+        public string AuxiliaryFunctionName(int index)
+        {
+            if (index == 0)
+                return "Specify Times";
+            return null;
+        }
+
+        public string AuxiliaryFunctionHelp(int index)
+        {
+            if (index == 0)
+                return "Specifies time points in the future at which forecasts will be generated.";
+            return null;
+        }
+
+        public bool AuxiliaryFunction(int idx, out object output)
+        {
+            output = null;
+            switch (idx)
+            {
+                case 0:
+                    var ts = GetInput(1) as TimeSeries;
+                    if (ts == null)
+                        return false;
+
+                    var interval = ts.GetCommonSamplingInterval();
+                    var first = ts.GetLastTime() + interval;
+                    var last = ts.GetLastTime() + new TimeSpan(interval.Ticks*20);
+                    var futureForm = new ForecastRangeForm(first, last, interval, true)
+                                         {StartPosition = FormStartPosition.CenterParent};
+                    if (futureForm.ShowDialog() == DialogResult.OK)
+                    {
+                        List<DateTime> range = futureForm.GetRange();
+                        FutureTimes = new DateTime[range.Count];
+                        for (int i = 0; i < range.Count; ++i)
+                            FutureTimes[i] = range[i];
+                        return true;
+                    }
+                    break;
+            }
+            return false;
+        }
+
+        public override List<Type> GetAllowedInputTypesFor(int socket)
+        {
+            if (socket == 0)
+                return new List<Type> { typeof(Model) };
+            if (socket == 1)
+                return new List<Type> { typeof(TimeSeries), typeof(MVTimeSeries) };
+            throw new SocketException();
+        }
+
+        public override List<Type> GetOutputTypesFor(int socket)
+        {
+            if (socket >= NumOutputs())
+                throw new SocketException();
+            return new List<Type> { typeof(TimeSeries), typeof(MVTimeSeries) };
+        }
+    }
+}
--- timeseries/TimeSeries.cs
+++ timeseries/TimeSeries.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using ARIMA.dataprocessing;
 using ARIMA.timeseries.stats;
 using MathNet.Numerics.LinearAlgebra;
+using ARIMA.cronos-ARMA.ABMath.ModelFramework;
+using ARIMA.timeseries.math.linalg;
+using cronos-ARMA.ABMath.ModelFramework.Transforms;
+
 
 namespace ARIMA.timeseries
 {
@@ -58,6 +62,91 @@ namespace ARIMA.timeseries
             DataObject[,] series = getSeries(data, len);
             Console.WriteLine(testStationarity(series, siglevel));
             //for (int i = 0; i < 50; i++)
+
+
+            int d = 0;
+            if (testStationarity(series, siglevel))
+            {
+                d = 1;
+            }
+
+            var tSeries = series;
+            
+            //Difference the series
+            if (d != 0)
+            {
+                tSeries = ArrayManipulation.diff(GetCol(tSeries, 1));
+            }
+
+            var ts = new Data.TimeSeries 
+                                {
+                                    Title = "Time Series",
+                                    Description = "TS Description"
+                                };
+            for (int i = 0, i < series.Count; i++)
+            {
+                ts.Add(tSeries[i,0], tSeries[i,1], false); //datetime, value, false
+            }
+
+            //TODO log transform the time series
+        
+            //Use ACF and PACF to find ARMA parameters
+            var highInterval = 1.96/Math.Sqrt(series.Count);
+            var acf = ts.ComputeACF(20, false);
+            bool crossed = acf[0] > highInterval;
+            int p = 0;
+            for (int i; i < acf.Count; i++) 
+            {
+                if ((acf[i] > highInterval) != crossed)
+                {
+                    p = i;
+                }
+            }
+            var pacf = ts.GetPACFFrom(acf);
+            crossed = pacf[0] > highInterval;
+            int q = 0;
+            for (int i; i < pacf.Count; i++);
+            {
+                if ((pacf[i] > highInterval) != crossed)
+                {
+                    q = i;
+                }
+            }
+
+            var model = ARMAModel(p, q);
+            model.theData = ts;
+            model.FitByMLE(200, 100, 0, null);
+
+            var forecaster = new ForecastTransform();
+            var futureTimes = new List<DateTime>();
+            var nextTime = ts.GetLastTime();
+            var daysProjected = 8
+            for (int t = 0; t < daysProjected; ++t )
+            {
+                nextTime = nextTime.AddDays(1);
+                futureTimes.Add(nextTime);
+            }
+            forecaster.FutureTimes = futureTimes.ToArray();
+
+            forecaster.SetInput(0, model, null); 
+            forecaster.SetInput(1, simulatedData, null);
+
+            var predictors = forecaster.GetOutput(0) as TimeSeries;
+
+            // now predictors is a time series of the forecast values for the next 8 days
+            // that is, predictors[0] is the predictive mean of X_{101} given X_1,...,X_100,
+            //          predictors[1] is the predictive mean of X_{102} given X_1,...,X_100, etc.
+        }
+
+        public static T[] GetCol<T>(this T[,] matrix, int col)
+        {
+            var colLength = matrix.GetLength(0);
+            var colVector = new T[colLength];
+
+            for (var i = 0; i < colLength; i++)
+            colVector[i] = matrix[i, col];
+
+            return colVector;
         }
 
         public DataObject[,] getSeries(List<string[]> data, int len)
